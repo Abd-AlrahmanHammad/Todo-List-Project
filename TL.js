c10df367629eccee7284cf05cef0f2c6f437fcd1
Abd-alrahman Hammad <aboodyaser334@gmail.com>
2025-06-22 18:02:56 +0300

    updata function

--- TL.js
+++ TL.js
@@ -89,6 +89,26 @@ function deleteTodo(index) {
     currentDeleteIndex = index;
     document.getElementById('deleteModal').style.display = 'flex';
 }
+function confirmDeleteTodo() {
+    todos.splice(currentDeleteIndex, 1);
+    closeDeleteModal();
+    msgshow("Task has been deleted.");
+    saveTodos();
+    render();
+}
+function deleteDoneTodos() {
+    todos = todos.filter(todo => !todo.done);
+    saveTodos();
+    render();
+}
+function deleteDoneTodos() {
+    const doneTasks = todos.filter(todo => todo.done);
+    if (doneTasks.length === 0) {
+        msgshow("No done tasks to delete.");
+        return;
+    }
+    document.getElementById('deleteAllDoneModal').style.display = 'flex';
+}
 function deleteAllTodos() {
     todos = [];
     saveTodos();
